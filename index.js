f803621a6bbbbe911b36e7a4e9bd3ea385487efe
Ashish5692 <ashishyadav5692@gmail.com>
2023-02-19 20:41:58 +0530

    setup of view wngine

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = 8000;
 //do routes it bydefault fetches index
 app.use('/', require('./routes'));
 
+//setting up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
